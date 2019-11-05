231f816c67ab438872bbcf11215e08287377c3df
Mac <mac@MacOnes-MacBook-Pro.local>
2019-11-05 13:03:05 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ReactReader, EpubView } from "react-reader";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [url, setUrl] = useState(
+    "/Users/mac/Desktop/projects/read-ebook/src/Chuong-8.epub"
   );
-}
+  return (
+    <>
+      <div style={{ /*position: "relative",*/ height: "100%" }}>
+        {" "}
+        <ReactReader
+          url={url}
+          title={"Alice in wonderland"}
+          location={"epubcfi(/6/2[cover]!/6)"}
+          locationChanged={epubcifi => console.log(epubcifi)}
+        />
+      </div>{" "}
+      <div style={{ /*position: "relative",*/ height: "100%" }}>
+        <EpubView
+          url={url}
+          location={"epubcfi(/6/2[cover]!/6)"}
+          locationChanged={epubcifi => console.log(epubcifi)}
+          tocChanged={toc => console.log(toc)}
+        />
+      </div>
+    </>
+  );
+};
 
 export default App;
